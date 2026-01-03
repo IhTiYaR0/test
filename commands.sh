e3ac9a81383f96befdcc4a385c973a337d3fef36
IhTiYaR0 <ytpolyus@gmail.com>
2026-01-04 02:14:35 +0500

    .122213

--- commands.sh
+++ commands.sh
@@ -1,8 +1,10 @@
-sudo rm -rf /etc/default/grub
-sudo cp -r ~/test/grub /etc/default/
+# sudo rm -rf /etc/default/grub
+# sudo cp -r ~/test/grub /etc/default/
 
 sudo rm -rf /etc/mkinitcpio.conf
 sudo cp -r ~/test/mkinitcpio.conf /etc/
 
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+# sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo mkinitcpio -P
+
+sudo pacman -Syu nvidia-dkms nvidia-utils lib32-nvidia-utils
