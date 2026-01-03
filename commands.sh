eb76b512da5ca33ab1464ce42d0cd9747ba0a667
IhTiYaR0 <ytpolyus@gmail.com>
2026-01-04 01:54:53 +0500

    .1222

--- commands.sh
+++ commands.sh
@@ -3,5 +3,5 @@
 # sudo pacman -S nvidia-dkms nvidia-utils
 
 # sudo mkinitcpio -P
-sudo rm -rf ~/.config/hypr/hyprland.conf 
-cp ~/test/hyprland.conf ~/.config/hypr/
+sudo pacman -Syu xorg-xwayland wayland-protocols
+
