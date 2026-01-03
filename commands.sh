02f71294202920f4ba15bc1181263c79e78a03ec
IhTiYaR0 <ytpolyus@gmail.com>
2026-01-04 02:05:24 +0500

    .12221

--- commands.sh
+++ commands.sh
@@ -1,4 +1,1 @@
-yay -S linux-headers nvidia-dkms qt5-wayland qt5ct libva libva-nvidia-driver-git
 sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-sudo sh -c 'echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia.conf'
-sudo reboot
