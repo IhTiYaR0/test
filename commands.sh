fe9179c0ad855b6021ed13069624cb6aeee8747d
IhTiYaR0 <ytpolyus@gmail.com>
2026-01-04 01:50:26 +0500

    .122

--- commands.sh
+++ commands.sh
@@ -1,6 +1,7 @@
-cp -r cod.sh ~/
 
-sudo pacman -Syu
-sudo pacman -S nvidia-dkms nvidia-utils
+# sudo pacman -Syu
+# sudo pacman -S nvidia-dkms nvidia-utils
 
-sudo mkinitcpio -P
+# sudo mkinitcpio -P
+sudo rm -rf ~/.config/hypr/hyprland.conf 
+cp ~/test/hyprland.conf ~/.config/hypr/
